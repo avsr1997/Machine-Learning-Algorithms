c3ad26c54ac0758d7c71fcfcd30290c77da5355f
Ajayveer Singh <38112090+avsr1997@users.noreply.github.com>
2018-06-06 20:56:46 +0530

    Add files via upload

---
+++ wine_dataset.py
@@ -0,0 +1,27 @@
+# Sample Program to Train Classifier on Wine Dataset
+
+from sklearn import tree
+from sklearn.datasets import load_wine
+import numpy as np
+test_id=[10,100,170]
+wine=load_wine()
+train_data=np.delete(wine.data,test_id,axis=0)
+train_target=np.delete(wine.target,test_id)
+test_data=wine.data[test_id]
+test_target=wine.target[test_id]
+clf=tree.DecisionTreeClassifier()
+clf.fit(train_data,train_target)
+print(clf.predict(test_data))
+
+# Generating a pdf file of Decision Tree on Wine Dataset
+
+from sklearn.externals.six import StringIO
+import pydotplus
+dot_data=StringIO()
+tree.export_graphviz(clf,out_file=dot_data,
+                     feature_names=wine.feature_names,
+                     class_names=wine.target_names,
+                     filled=True,rounded=True,
+                     impurity=False)
+graph=pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("wine_tree.pdf")
