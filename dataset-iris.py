f8643714553299d445ecf5d5dc9dbf6b7c8ec1db
Ajayveer Singh <38112090+avsr1997@users.noreply.github.com>
2018-06-08 14:04:53 +0530

    Update and rename iris_dataset.py to dataset-iris.py

--- dataset-iris.py
+++ dataset-iris.py
@@ -1,19 +1,21 @@
 #Sample Program to Train Classifier on Iris Dataset
-
 from sklearn import tree
 from sklearn.datasets import load_iris
 import numpy as np
+
+# Test data is created in a list
 test_id=[0,50,100]
 iris=load_iris()
 train_data=np.delete(iris.data,test_id,axis=0)
 train_target=np.delete(iris.target,test_id)
 test_data=iris.data[test_id]
 test_target=iris.target[test_id]
+
+# Tree classifier is used to train the whole dataset
 clf=tree.DecisionTreeClassifier()
 clf.fit(train_data,train_target)
 
 #Generating a pdf file of Decision Tree on Wine Dataset
-
 from sklearn.externals.six import StringIO
 import pydotplus
 dot_data=StringIO()
