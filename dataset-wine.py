dda139f5e63c703ce4ca64949baff988be228dbb
Ajayveer Singh <38112090+avsr1997@users.noreply.github.com>
2018-06-08 14:09:00 +0530

    Update and rename wine_dataset.py to dataset-wine.py

--- dataset-wine.py
+++ dataset-wine.py
@@ -1,20 +1,22 @@
 # Sample Program to Train Classifier on Wine Dataset
-
 from sklearn import tree
 from sklearn.datasets import load_wine
 import numpy as np
+
+# Test data is created in list
 test_id=[10,100,170]
 wine=load_wine()
 train_data=np.delete(wine.data,test_id,axis=0)
 train_target=np.delete(wine.target,test_id)
 test_data=wine.data[test_id]
 test_target=wine.target[test_id]
+
+# Tree classifier is used to train thw dataset
 clf=tree.DecisionTreeClassifier()
 clf.fit(train_data,train_target)
 print(clf.predict(test_data))
 
 # Generating a pdf file of Decision Tree on Wine Dataset
-
 from sklearn.externals.six import StringIO
 import pydotplus
 dot_data=StringIO()
